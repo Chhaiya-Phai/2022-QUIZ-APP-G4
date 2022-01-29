4c9dbf1344fdeafedf30b7c9c1695d167b61233e
lidathy-real <lidathy9@gmail.com>
2022-01-29 12:27:37 +0700

    edit code

--- js/edit-quiz.js
+++ js/edit-quiz.js
@@ -96,6 +96,15 @@ function addQuestion() {
     } else {
         // itnot valid display error
         // TODO
+        Swal.fire({
+            title: 'Custom animation with Animate.css',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
         
     }
 };
